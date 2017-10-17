0d6d04f2e31c1b9c3d6bfc141140588c778b452c
oleh Mykytsei <olegmkcw@gmail.com>
2017-10-17 16:10:49 +0300

    Version 0.23 JQuery

--- jq_todolist/scripts/scripts.js
+++ jq_todolist/scripts/scripts.js
@@ -1,27 +1,50 @@
 $(document).ready(function(){
 	
 	$("#btnadd").click(function(){
-		if ($("#text").val() == "") return;
-		$("#task").append("<li>" + $("#text").val() + "</li>");
-		$("#text").val("");
+		if ($("#texttask").val() == "") return;
+		$("#task").append("<li>" + $("#texttask").val() + "</li>");
+	//	$("#task").append(divtext());
+		$("#texttask").val("");
 	})
 
-	/*$("#task").on("click", "li", function() {
+	$("#task").on("click", "li", function() {
         $(this).toggleClass("done");
-     });*/*
-
-     $("#task").on("click", "li", function() {
-        $(this).addClass("done");
      });
+
+    /*$("#task").on("click", "li", function() {
+        $(this).addClass("done");
+    });*/
 
 	
 	$("#btndone").click(function(){
-		$("#task").toggleClass("done")
+		$("#task").toggleClass("done");
 	});
 
-	$("#reset").click(function(){
+	/*$("#btndone").click(function(){
+		$("#task").addClass("done");
+	});*/
+
+	/*$("#btnundone").click(function(){
+		$("#task").addClass("undone");
+	});*/
+
+	$("#delete").click(function(){
 		$("li").hide("slow");
 	});	
+
+	
+
+	/*function divtext(){
+	$(".accordion h3:first").addClass("active");
+	$(".accordion p:not(:first)").hide();
+
+	$(".accordion h3").click(function(){
+		$(this).next("p").slideToggle("slow").siblings("p:visible").slideUp("slow");
+		$(this).toggleClass("active");
+		$(this).siblings("h3").removeClass("active");
+
+	});
+	};*/
 
 
 
